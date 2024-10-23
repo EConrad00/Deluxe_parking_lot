e7b4c61ab9f0375f9b8400920485806818bc2329
EConrad00 <emil.conrad@campusnykoping.se>
2024-10-23 17:15:03 +0200

    Basic loop to run program

--- Deluxe_parking_lot/Deluxe_parking_lot/Program.cs
+++ Deluxe_parking_lot/Deluxe_parking_lot/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class MainProgram
+{
+    static void Main(string[] args) 
+    {
+        while (true)
+        {
+            Console.WriteLine("[T]Test code\r\n" + "[E]xit\r\n");
+            string key = Console.ReadKey().Key.ToString();
+            switch (key) 
+            {
+                case "T":
+                    break;
+                case "E":
+                    Console.Clear();
+                    return;
+            }
+
+        }
+
+    }
+    
+
+}
